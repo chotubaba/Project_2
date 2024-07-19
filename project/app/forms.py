266b607604f7ca5a97a86e23eff614ac11b81c84
bhardwajkiah <kiahkatyayani@gmail.com>
2024-07-19 13:14:31 +0300

    Fix: models,py: Customise save method to include email

--- project/app/forms.py
+++ project/app/forms.py
@@ -11,3 +11,10 @@ class CreateUserForm(UserCreationForm):
     Generate a form based on the User model with the fields:
     username, email, password1, password2
     '''
+    # # CUSTOMISE SAVE METHOD TO INCLUDE 'EMAIL' FIELD
+    def save(self, commit = True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data('email')
+        if commit:
+            user.save()
+        return user
